c750d7b66b826478991bd0cc5fa3400f5a65c79d
Neekon Saadat <neekonsu@gmail.com>
2020-07-14 08:06:34 -0700

    Merged docker image and scripts repo

--- stage1.go
+++ stage1.go
@@ -1,20 +1,19 @@
 package abcscripts
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 )
 
 // StageOne function runs stage1 of procedure described in README.md
-func StageOne() {
-	INPUTBAM := flag.String("INPUTBAM", "./example_chr22/input_data/Chromatin/wgEncodeUwDnaseK562AlnRep1.chr22.bam", "Path to input DNase-Seq or ATAC-Seq Bam file")
-	OUTPUTMACS2 := flag.String("OUTPUTMACS2", "./wgEncodeUwDnaseK562AlnRep1.chr22.macs2", "Path to MACS2 output file")
-	OUTPUTDIRECTORY := flag.String("OUTPUTDIRECTORY", "./example_chr22/ABC_output/Peaks/", "Path to general MACS2 output directory")
+func StageOne(INPUTBAM string, OUTPUTMACS2 string, OUTPUTDIRECTORY string) {
+	// INPUTBAM := flag.String("INPUTBAM", "./example_chr22/input_data/Chromatin/wgEncodeUwDnaseK562AlnRep1.chr22.bam", "Path to input DNase-Seq or ATAC-Seq Bam file")
+	// OUTPUTMACS2 := flag.String("OUTPUTMACS2", "./wgEncodeUwDnaseK562AlnRep1.chr22.macs2", "Path to MACS2 output file")
+	// OUTPUTDIRECTORY := flag.String("OUTPUTDIRECTORY", "./example_chr22/ABC_output/Peaks/", "Path to general MACS2 output directory")
 	stage1 := &exec.Cmd{
 		Path:   "./stage1.sh",
-		Args:   []string{*INPUTBAM, *OUTPUTMACS2, *OUTPUTDIRECTORY},
+		Args:   []string{INPUTBAM, OUTPUTMACS2, OUTPUTDIRECTORY},
 		Stdout: os.Stdout,
 		Stderr: os.Stdout,
 	}
--- stage2.go
+++ stage2.go
@@ -1,1 +1,6 @@
 package abcscripts
+
+// Stage2 takes in parameters and executes stage two of the pipeline as described in ABC_procedure.txt
+func Stage2() {
+
+}

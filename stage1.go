5db0cba8ce7d8db2c4644db2fcb5cbaea3180bd6
Neekon Saadat <neekonsu@gmail.com>
2020-07-14 11:23:55 -0700

    Commented stage 1

--- stage1.go
+++ stage1.go
@@ -7,13 +7,19 @@ import (
 )
 
 // StageOne function runs stage1 of procedure described in README.md
-func StageOne(INPUTBAM string, OUTPUTMACS2 string, OUTPUTDIRECTORY string) {
+func StageOne(INPUTBAM string,
+	OUTPUTMACS2 string,
+	OUTPUTDIRECTORY string,
+	REFERENCECHROMOSOMEDIRECTORY string,
+	ABCREPOSITORYPYTHONDIRECTORY string) {
+	// Spawn shell command for `stage1.sh` script
 	stage1 := &exec.Cmd{
 		Path:   "./stage1.sh",
-		Args:   []string{INPUTBAM, OUTPUTMACS2, OUTPUTDIRECTORY},
+		Args:   []string{INPUTBAM, OUTPUTMACS2, OUTPUTDIRECTORY, REFERENCECHROMOSOMEDIRECTORY},
 		Stdout: os.Stdout,
 		Stderr: os.Stdout,
 	}
+	// Generic error handling
 	if err := stage1.Run(); err != nil {
 		fmt.Println(err)
 	}
--- stage1.sh
+++ stage1.sh
@@ -1,16 +1,45 @@
 #!/bin/sh
 
+# Base filename for the input bam file it MACS2 (sliced extension)
+INPUT="${$0%.*}"
+# Input bam file for MACS2
 INPUTBAM=$0
+# Output filename for MACS2 file
 OUTPUTMACS2=$1
+# Output directory for MACS2, input for bedtools
 OUTPUTDIRECTORY=$2
+# Directory of reference chromosome, located inside ABC git repo (example_chr22/reference/chr22)
+REFERENCECHROMOSOMEDIRECTORY=$3
+# Directory of all python scripts/sourcecode in ABC git repo (src/*.py)
+ABCREPOSITORYPYTHONDIRECTORY=$4
 
+# Confirming that arguments are passed correctly between go and shell through printout
 echo "0: $0, 1: $1, 2: $2" >&2
 
+# Call peaks on a DNase-seq or ATAC-seq bam file using MACS2
 macs2 callpeak \
-            -t $INPUTBAM \
-            -n $OUTPUTMACS2 \
-            -f BAM \
-            -g hs \
-            -p .1 \
-            --call-summits \
-            --outdir $OUTPUTDIRECTORY
+    -t $INPUTBAM \
+    -n $OUTPUTMACS2 \
+    -f BAM \
+    -g hs \
+    -p .1 \
+    --call-summits \
+    --outdir $OUTPUTDIRECTORY
+
+# Sort narrowPeak file using bedtools
+bedtools sort -faidx $REFERENCECHROMOSOMEDIRECTORY \
+    -i $OUTPUTDIRECTORY/$INPUT.macs2_peaks.narrowPeak 
+    > $OUTPUTDIRECTORY/$INPUT.macs2_peaks.narrowPeak.sorted
+
+# Define candidate regions using output of sorted ^narrowPeaks^
+# May need to change virtual environments here
+# `nStrongestPeaks` needs calibration. Read ABC documentation for commentary.
+python src/makeCandidateRegions.py \
+    --narrowPeak example_chr22/ABC_output/Peaks/wgEncodeUwDnaseK562AlnRep1.chr22.macs2_peaks.narrowPeak.sorted \
+    --bam example_chr22/input_data/Chromatin/wgEncodeUwDnaseK562AlnRep1.chr22.bam \
+    --outDir example_chr22/ABC_output/Peaks/ \
+    --chrom_sizes example_chr22/reference/chr22 \
+    --regions_blacklist reference/wgEncodeHg19ConsensusSignalArtifactRegions.bed \
+    --regions_whitelist example_chr22/reference/RefSeqCurated.170308.bed.CollapsedGeneBounds.TSS500bp.chr22.bed \
+    --peakExtendFromSummit 250 \
+    --nStrongestPeaks 3000

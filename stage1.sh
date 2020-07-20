50c6f8a73ac7b9d27e4d76f2fbeea2a463ac8b11
Neekon Saadat <neekonsu@gmail.com>
2020-07-20 15:44:05 -0700

    same

--- stage1.sh
+++ stage1.sh
@@ -29,12 +29,13 @@ WD: ${12}
 # Confirming that arguments are passed correctly between go and shell through printout
 echo "——————————————————————" >&2
 echo "Confirming arguments:" >&2
-echo "1: $1,\n2: $2,\n3: $3,\n4: $4,\n5: $5,\n6: $6,\n7: $7,\n8: $8,\n9: $9,\n10: ${10},\n11: ${11},\n ${12}" >&2
+echo "1: $1\n2: $2\n3: $3\n4: $4\n5: $5\n6: $6\n7: $7\n8: $8\n9: $9\n10: ${10}\n11: ${11}\n12: ${12}" >&2
 echo "——————————————————————" >&2
 echo "Wait 3 seconds . . . "
 sleep 3
 echo "Setting working directory to ${12}"
 cd $WD
+pwd
 
 # Call peaks on a DNase-seq or ATAC-seq bam file using MACS2
 macs2 callpeak \

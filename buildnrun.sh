585793a053432630a64220058658ae6394812a39
Neekon Saadat <neekonsu@gmail.com>
2020-07-20 19:28:42 -0700

    let's us see if conda works

--- buildnrun.sh
+++ buildnrun.sh
@@ -3,6 +3,6 @@
 git add . && \
 git commit -m "$1" && \
 git push origin master && \
-docker container rm -f $(docker ps -aq) && \
+# docker container rm -f $(docker ps -aq) && \
 docker build . -t neekonsu/abc_pipeline:latest && \
 docker container run -it --gpus all --name main neekonsu/abc_pipeline:latest

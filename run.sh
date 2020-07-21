697415a2ad0f077774a132c34320f04507a99f46
Neekon Saadat <neekonsu@gmail.com>
2020-07-21 11:03:11 -0700

    reincorporated conda environment inside ABC-...

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-conda init bash && \
-. "/root/.bashrc" && \
-# conda env create -f "./alphabet/conda.yml" && \
-# conda env activate zerpzorp && \ 
 cd alphabet && \
 git pull origin master && \
 go run "main/main.go"

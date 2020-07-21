7c9ca58d5f2cfc9b37b1063a6b989f65f3f001c2
Neekon Saadat <neekonsu@gmail.com>
2020-07-21 10:51:00 -0700

    removed conda environment

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 conda init bash && \
 . "/root/.bashrc" && \
-conda env create -f "./alphabet/conda.yml" && \
-conda env activate zerpzorp && \
+# conda env create -f "./alphabet/conda.yml" && \
+# conda env activate zerpzorp && \
 cd alphabet && \
 git pull origin master && \
 go run "main/main.go"

1db1d136a0eece23fdb5b74f29d82d3f4368ae86
neekonsu <neekonsu@gmail.com>
2020-08-22 14:20:54 -0700

    Reorganized repository

--- helper/buildnrun.sh
+++ helper/buildnrun.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build . -t neekonsu/abc_pipeline:latest && \
+docker build  -t neekonsu/abc_pipeline:latest && \
 docker container run -it --gpus all --name main neekonsu/abc_pipeline:latest

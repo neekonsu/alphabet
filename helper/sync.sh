513e605a74c263383d29b469b0f5d7556aaf7b0b
neekonsu <neekonsu@gmail.com>
2020-08-23 11:10:05 -0700

    Debugging makeCandidateRegions.py

--- helper/sync.sh
+++ helper/sync.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 go build -o main/alphabet -i main/main.go
 chmod +x main/alphabet
-git submodule update --init
+git submodule foreach git add -A
+git submodule foreach git commit -m "$1"
+git submodule foreach git push origin master
 git add .
 git commit -m "$1"
 git push --recurse-submodules=on-demand origin master

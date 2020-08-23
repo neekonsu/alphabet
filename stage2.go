ab5fae70a1318c86d011fc09e5d9b4ef75ad35d6
neekonsu <neekonsu@gmail.com>
2020-08-23 10:34:55 -0700

    Debugging makeCandidateRegions.py

--- stage2.go
+++ stage2.go
@@ -8,15 +8,15 @@ import (
 
 // StageTwo function runs stage2 of procedure described in README.md
 func StageTwo(args *[]string) {
-	// Spawn shell command for `stage2.sh` script
-	stage2 := &exec.Cmd{
+	// Spawn shell command for `stage1.sh` script
+	stage1 := &exec.Cmd{
 		Path:   "/alphabet/stage2.sh",
 		Args:   (*args),
 		Stdout: os.Stdout,
 		Stderr: os.Stdout,
 	}
 	// Generic error handling
-	if err := stage2.Run(); err != nil {
+	if err := stage1.Run(); err != nil {
 		fmt.Println(err)
 	}
 }

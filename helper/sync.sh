f1912de1964bcf0d898583d05c80334c591b57be
neekonsu <neekonsu@gmail.com>
2020-08-22 20:19:02 -0700

    Debugging makeCandidateRegions.py

--- helper/sync.sh
+++ helper/sync.sh
@@ -2,5 +2,6 @@
 go build -o main/alphabet -i main/main.go
 chmod +x main/alphabet
 git submodule update --init
-git commit -am "$1"
+git add .
+git commit -m "$1"
 git push --recurse-submodules=on-demand origin master
---
+++ markdown/TODOS.md
@@ -0,0 +1,5 @@
+# TODOS
+## Mandatory
+### Debug makeCandidateRegions.py
+## Soft-TODOs
+### Test makeCandidateRegions.py now that shebang points to bash instead of sh 
